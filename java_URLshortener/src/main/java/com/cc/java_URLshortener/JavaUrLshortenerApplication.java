821e003a91fb6f7be646507bdcf4bc766c535b09
CustCoding <custodiotitossetitosse@gmail.com>
2026-04-03 05:22:44 +0200

    chore(java): initialize spring project URL shortener

---
+++ java_URLshortener/src/main/java/com/cc/java_URLshortener/JavaUrLshortenerApplication.java
@@ -0,0 +1,13 @@
+package com.cc.java_URLshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUrLshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUrLshortenerApplication.class, args);
+	}
+
+}
