9a33dcc32947700784fd17a8d0217253ed9c3a40
CustCoding <custodiotitossetitosse@gmail.com>
2026-04-03 05:19:03 +0200

    feat(java): add url validator

---
+++ java_URLshortener/src/main/java/com/cc/java_URLshortener/util/UrlValidator.java
@@ -0,0 +1,17 @@
+package com.cc.java_URLshortener.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UrlValidator {
+    public static boolean isValid(String url){
+
+        try{
+            new URL(url);
+            return true;
+        } catch (MalformedURLException e){
+            return false;
+        }
+
+    }
+}
